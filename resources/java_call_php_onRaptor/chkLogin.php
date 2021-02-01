f668ec42a4a943dbac51ae7caef6708af2bcc479
jl749 <jl749@kent.ac.uk>
2021-02-01 11:18:15 +0000

    java code to call php on raptor

---
+++ resources/java_call_php_onRaptor/chkLogin.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "dragon.kent.ac.uk";
+$username = "jl749";
+$password = "p@ssword1";
+$dbname = "jl749";
+
+ 
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if(!$conn) {
+	die('MySQL ERROR: ' . mysql_error());
+}
+ 
+if(isset($_POST["id"]) && $_POST["pass"]){
+	$id = $_POST["id"];
+	$pass = $_POST["pass"];
+
+	$sql = "SELECT * FROM User_Info WHERE Username='$id' AND Password='$pass'";
+	$result = mysqli_query($conn, $sql); //$conn->query($sql)
+
+	if (mysqli_num_rows($result) > 0) { //$result->num_rows
+		echo "True";
+	}else{
+		echo "False";
+	}
+
+	mysqli_close($conn);
+}
+?>
---
+++ resources/java_call_php_onRaptor/src/Main.java
@@ -0,0 +1,59 @@
+//import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class Main {
+	private static final String raptor="https://raptor.kent.ac.uk/~jl749/";
+	
+	public static boolean chkLogin(String id, String pass) {
+		StringBuilder result=new StringBuilder();
+		
+		OutputStream out=null;
+		InputStreamReader in=null;
+		BufferedReader reader=null;
+		try {
+			URL url = new URL(raptor+"chkLogin.php");
+			String msg="id="+id+"&pass="+pass;
+			byte[] postDataBytes=msg.getBytes("UTF-8");
+			
+			URLConnection con = url.openConnection();
+			HttpsURLConnection https = (HttpsURLConnection)con;
+			https.setRequestMethod("POST");
+			https.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			https.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+			https.setDoOutput(true);
+			out=https.getOutputStream();
+			out.write(postDataBytes);
+			
+			in=new InputStreamReader(https.getInputStream(),"UTF-8");
+			reader=new BufferedReader(in);
+			result.append(reader.readLine());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			//con.close();
+			try{reader.close();}catch(Exception e) {e.printStackTrace();}
+			try{in.close();}catch(Exception e) {e.printStackTrace();}
+			try{out.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		System.out.println(result.toString()+"!");
+		if(result.toString().equals("True")) 
+			return true;
+		else 
+			return false;
+	}
+	
+	public void updateTMP(double val) {
+		
+	}
+	
+	public static void main(String[] args) {
+		chkLogin("John98","19513FDC9DA4FB72A4A05EB66917548D3C90FF94D5419E1F2363EEA89DFEE1DD");
+	}
+}
