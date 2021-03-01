173d1b2e71619ddfca7df22cadc1d784d3c79ba6
jl749 <jl749@kent.ac.uk>
2021-03-01 15:53:02 +0000

    kent staff php esp8266

---
+++ resources/ARDUINO/ESP32_staffserver_TEST/ESP32_staffserver_TEST.ino
@@ -0,0 +1,66 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+WiFiClientSecure secureClient;
+
+const char* phpHost = "www.cs.kent.ac.uk/people/staff/ds710/co600/getThreshold.php";
+const char* ssid = "BT-FFA25W";
+const char* pass = "cATY9hbJUC4GQi";
+const char* root_ca= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n" \
+"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n" \
+"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n" \
+"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n" \
+"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n" \
+"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n" \
+"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n" \
+"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n" \
+"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n" \
+"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n" \
+"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n" \
+"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n" \
+"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n" \
+"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n" \
+"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n" \
+"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n" \
+"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n" \
+"-----END CERTIFICATE-----\n";
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, pass);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  
+  String data="houseID=1234";
+  secureClient.setCACert(root_ca); //loadCACert
+  WiFi.mode(WIFI_STA);
+  if(secureClient.connect(phpHost, 443)){
+    Serial.println(data);
+    secureClient.println("POST " + (String)phpHost + " HTTP/1.1");
+    secureClient.println("Host: " + (String)phpHost);
+    secureClient.println("User-Agent: ESP8266/1.0");
+    secureClient.println("Connection: close");
+    secureClient.println("Content-Type: application/x-www-form-urlencoded;");
+    secureClient.print("Content-Length: ");
+    secureClient.println(data.length());
+    secureClient.println();
+    secureClient.println(data);
+    delay(100);
+    String response = secureClient.readString();
+    Serial.println(response);
+  }else {
+    Serial.println("ERROR");
+  }
+}
+
+void loop() {
+  
+}
